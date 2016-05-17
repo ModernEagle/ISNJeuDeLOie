8f1b7b7e3c600ec36ea3a7dcf66854667a69fbae
Knoppix User <knoppix@georges.khaznadar.fr>
2016-05-17 12:57:34 +0200

    lol

--- Serveur/Serveur.py
+++ Serveur/Serveur.py
@@ -1,38 +1,50 @@
 import os, os.path
 import random
 import string
-
+import json
 import cherrypy
-
+resultat=0
+pos1 = 54
+pos2 = 630
 class StringGenerator(object):
    @cherrypy.expose
    def index(self):
        return """<html>
          <head>
            <link href="/static/css/style.css" rel="stylesheet">
+           <script type= "text/javascript" src="/static/jquery-2.1.0.min.js"> </script>
+           <title>Jeu de l'Oie ISN</title>
    </head>
 		<body>
 			<img SRC="/static/plateauofficial.jpg"
 			alt="plateau du jeu de l'oie" />
-		<img SRC="/static/sprite.jpeg";  style="left:100px; top:100px; position:absolute;" style="width="60"; height="120";
+		<img SRC="/static/skieur1.png";  style="left:56px; top:640px; position:absolute;" style="width="40"; height="60"; draggable=true
 		alt="image de sprite" />
-		<img SRC="/static/sprite.jpeg"; style="left:800px; top:500px; position:absolute; style="width="100"; height="180";
+		<img SRC="/static/skieur2.png";  style="left:0px; top:640px; position:absolute;" style="width="40"; height="60"; draggable=true
 		alt="image de sprite" />
-		<img SRC="/static/sprite.jpeg"; style="left:250px; top:300px; position:absolute;" style="width="50"; height="80"; 
+		<img SRC="/static/skieur3.png";  style="left:56px; top:590px; position:absolute;" style="width="40"; height="60"; draggable=true
 		alt="image de sprite" />
-		</body>
+		<img SRC="/static/skieur4.png";  style="left:0px; top:590px; position:absolute;" style="width="40"; height="60"; draggable=true
+		alt="image de sprite" />
+		
 	</html>
-<input type="button" class="bouton_actif" id="b1" value="lancer le de">"""
-
+<input type="button" class="bouton_actif" id="b1" value="lancer le dé" onclick="$.get('/lancedede').done(
+                function(data){
+                  console.log(data);
+                  $('#resultat').html(data);
+                })">
+                <div id="resultat">
+                </div>
+"""
    @cherrypy.expose
-   def generate(self, length=8):
-       some_string = ''.join(random.sample(string.hexdigits, int(length)))
-       cherrypy.session['mystring'] = some_string
-       return some_string
+   @cherrypy.tools.json_out()
+   def lancedede(self):
+	   typD = 6
+	   nbD = 1
+	   resultat = (random.randrange(1,(typD+1)))
+	   return (resultat)
 
-   @cherrypy.expose
-   def display(self):
-       return cherrypy.session['mystring']
+  
 
 conf = {
         '/': {
@@ -44,6 +56,8 @@ conf = {
             'tools.staticdir.dir': './public'
         }
     }
+    
+
 cherrypy.quickstart(StringGenerator(), '/', "serveur.conf")
 
 
